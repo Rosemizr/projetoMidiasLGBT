cf991acad459e4605eeedc4e7482d89e11a1fd3d
Rosemizr <otapatilera@gmail.com>
2023-07-24 11:33:30 -0300

    correção de erros

--- src/app.js
+++ src/app.js
@@ -14,4 +14,9 @@ mongoose.connect();
 app.use("/midiasLGBT/obras", midiasRoutes)
 app.use("/midiasLGBT/acesso", acessoRoutes)
 
+const swaggerUi = require('swagger-ui-express')
+const swaggerFile = require('../swagger/swagger_output.json')
+
+app.use('/minha-rota-de-documentacao', swaggerUi.serve, swaggerUi.setup(swaggerFile))
+
 module.exports = app
